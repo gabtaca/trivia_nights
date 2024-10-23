4d7b157d6bc1fb09a64f2f77d196a018f03cb0c0
Gabtaca <gabrieltaca117@gmail.com>
2024-10-23 12:52:05 -0400

    mobile acceuil wip bg done

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        
+    <div className="bg-brick-background bg-repeat bg-contain bg-[#31325D] w-full h-[100vh]">
+      <main className="flex flex-col justify-center items-center w-full h-full">
+        <div className="home_title flex flex-col items-center bg-[#2B0B38] bg-opacity-[79%] w-[90%] h-[80%] rounded-[50px]">
+          <div className="ctrl_title flex blur-[1px]">
+            <h1 className="font-tiltNeon text-[109px] text-shadow-neon-pink text-stroke-pink text-pink-100">TRIVIA</h1>
+            <h1 className="font-tiltNeon text-[109px] absolute text-pink-100">TRIVIA</h1>
+          </div>
+          <h2 className="">NIGHT</h2>
+        </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
 
